ae040fc619ff1532cdf47230870729c8fdf1a39f
Renn <reno75874@gmail.com>
2024-10-09 10:38:17 +0700

    add component layouts

---
+++ resources/views/components/partials/head.blade.php
@@ -0,0 +1,14 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <link rel="stylesheet" href="./assets/css/vendor/glightbox.css">
+  <link rel="stylesheet" href="./assets/css/vendor/plyr.css">
+  <link rel="stylesheet" href="./assets/css/vendor/prism.css">
+  <link rel="stylesheet" href="./assets/css/vendor/scrollcue.css">
+  <link rel="stylesheet" href="./assets/css/vendor/swiper-bundle.min.css">
+  <link rel="stylesheet" href="./assets/css/vendor/animate.css">
+
+  {{ $slot }}
+</head>
