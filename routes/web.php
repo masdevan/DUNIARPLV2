57e706219e0b0734bff8e01231fa67d0913603f2
MasDevan <masdevansugiharta@gmail.com>
2024-10-09 09:09:00 +0700

    New Update Simple Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
+// Main Laravel Routing
 Route::get('/', function () {return view('web.index');});
+Route::get('/login', function() {return view('log.login');})->name('sigin');
+Route::get('/signup', function() {return view('log.signup');})->name('signup');
 
-Route::get('/login', function() {return view('log.login');})->name('sigin'); 
-Route::get('/signup', function() {return view('log.signup');})->name('signup'); 
+// Main Admin Routing
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
